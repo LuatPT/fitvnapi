9af7b1a2521e234265cdc12d21295b21bd75687e
luatptbeta <luatphamdhv@gmail.com>
2021-05-24 17:12:05 +0700

    Fix config

--- src/main/java/com/api/controller/IndexController.java
+++ src/main/java/com/api/controller/IndexController.java
@@ -2,11 +2,13 @@ package com.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 	@RequestMapping(method = RequestMethod.GET, value = "/")
+	@ResponseBody
 	public String index () {
 		return "Welcome to My API";
 	}
