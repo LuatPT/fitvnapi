4e968126623d5365552297b89aa016e9a2938927
LuatPT <luatphamdhv@gmail.com>
2021-06-16 15:59:11 +0700

    add swagger ui

--- src/main/java/com/api/security/SecurityConfig.java
+++ src/main/java/com/api/security/SecurityConfig.java
@@ -16,37 +16,46 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.api.security.jwt.JwtAuthenticatonFilter;
 import com.api.service.UserService;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+@EnableSwagger2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	UserService userService;
-	
-	
+
 	@Bean
 	public JwtAuthenticatonFilter jwtAuthenticatonFilter() {
 		return new JwtAuthenticatonFilter();
 	}
-	
+
 	@Bean(BeanIds.AUTHENTICATION_MANAGER)
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		// get AuthenticationManager Bean
 		return super.authenticationManagerBean();
 	}
-	
-	@Bean 
+
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService) // Cung cáp userservice cho spring security
-		.passwordEncoder(passwordEncoder());// cung cấp password encoder
+				.passwordEncoder(passwordEncoder());// cung cấp password encoder
 	}
-	
+
 //	@Bean
 //	CorsConfigurationSource corsConfigurationSource() {
 //        CorsConfiguration configuration = new CorsConfiguration();
@@ -58,27 +67,50 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //        source.registerCorsConfiguration("/**", configuration);
 //        return source;
 //    }
-	
+
+	private static final String[] AUTH_WHITELIST = {
+			"/",
+			"/csrf",
+			"/v1/refreshtoken",
+			"/v1/login",
+			"/v1/register",
+			"/v1/foods/**",
+			"/v1/mealPlans/**",
+			"/v1/getMealPlans/**",
+			"/v1/exercisePlans/**",
+			"/v1/userInfos/**",
+	        "/swagger-resources/**",
+	        "/v1/getUserInfos/**",
+	        "/v1/getCaloMap/**",
+	        "/swagger-ui.html",
+	        "/v2/api-docs",
+	        "/v1/api-docs",
+	        "/webjars/**"
+	};
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.cors()
-		.and().csrf().disable()
-		.authorizeRequests()
-		.antMatchers("/").permitAll()
-		.antMatchers("/v1/login").permitAll() 
-		.antMatchers("/v1/register").permitAll()
-		.antMatchers("/v1/refreshtoken").permitAll()
-		.antMatchers("/v1/foods/**").permitAll()// everyone can access
-		.antMatchers("/v1/getMealPlans/**").permitAll()
-		.antMatchers("/v1/mealPlans/**").permitAll()// everyone can access
-		.antMatchers("/v1/exercisePlans/**").permitAll()// everyone can access
-		.antMatchers("/v1/getUserInfos/**").permitAll()
-		.antMatchers("/v1/userInfos/**").permitAll()
-		.antMatchers("/v1/getCaloMap/**").permitAll()
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.cors().and().csrf().disable().authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll()
 //		.antMatchers("/v1/exercises").hasAuthority(LoginRole.USER.getAuthority())
 		.anyRequest().authenticated();
 		// Thêm một lớp Filter kiểm tra jwt
 		http.addFilterBefore(jwtAuthenticatonFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
-		
+
+	@Bean
+	public Docket configure() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.api"))
+				.paths(PathSelectors.any()).build()
+				.apiInfo(apiEndPointsInfo());
+	}
+	private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("FIT REST API")
+                                   .description("Spring Boot REST API")
+                                   .contact(new Contact("LuatPT", "http://fitvn.herokuapp.com", "luatphamdhv@gmail.com"))
+                                   .license("Tomcat 9.0")
+                                   .licenseUrl("http://www.tomcat.org/licenses/LICENSE-9.0.html")
+                                   .version("1.0.0")
+                                   .build();
+    }
 }
